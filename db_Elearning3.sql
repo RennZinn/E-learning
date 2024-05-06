1c4c4bd5e43d2d9cf35f89ca9934b7fa3d65d653
Rennan <163460636+RennZinn@users.noreply.github.com>
2024-05-06 11:17:03 -0300

    Update db_Elearning3.sql

--- db_Elearning3.sql
+++ db_Elearning3.sql
@@ -1,15 +1,40 @@
-use db_pessoas2;
-create table  tb_pessoa(
-codigo int primary key auto_increment,
-nome varchar(50),
-fone varchar(15) not null,
-email varchar(50) not null
-);
+CREATE table CadastroUsuarios(
+ra int primary key auto_increment,
+cpf varchar(14) not null,
+rg varchar(12) not null,
+nomeCompleto varchar(50)not null,
+dataNascimento date not null,
+email varchar(40) not null,
+telefone varchar(14) not null,
+endereco varchar(30) not null,
+bairro varchar(20) not null,
+numero int not null,
+cidade varchar(20) not null,
+estado varchar(2) not null,
+pais varchar(20) not null,
+senha varchar(15) not null
+)
+select * from CadastroUsuarios;
 
-insert into tb_pessoa
-(nome, fone, email)
-values ("ede", "00213", "lasdhoo"),
-("1", "0", "@yahoo");
+Create table CadastroCurso(
+horasCurso int not null,
+horarioAula int not null,
+dataInicio date not null,
+dataAula date not null,
+materialCurso varchar(40) not null,
+professor varchar(40) not null
+)
+select * from CadastroCurso;
+
+
+Create table CadastroMaterial
+(
+numeroDeSerie int primary key auto_increment,
+genero varchar(15) not null,
+quantidadeMaterial int not null,
+posseLivro varchar(30),
+entragaPosse date not null
+)
 
 Create Table adm(
 codigoadm int primary key auto_increment,
